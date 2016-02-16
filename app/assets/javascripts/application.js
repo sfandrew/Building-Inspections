9942f54ef0979637d6e0a6853add1cd05a30166b
james <james@sfrent.net>
2016-02-16 11:25:28 -0800

    Integrating angular

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,5 +15,6 @@
 //= require underscore
 //= require bootstrap-sprockets
 //= require angular
+//= require angular-ui-router
 //= require angular-rails-templates
 //= require_tree .
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,16 +1,129 @@
-var inspectionApp = angular.module('buildingInspections', ['templates']);
+var inspectionApp = angular.module('buildingInspections', ['templates', 'ui.router']);
+
+inspectionApp.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('home');
+
+  $stateProvider.state('home', {
+    url: "/home",
+    templateUrl: '/assets/home.html',
+    controller: 'HomeCtrl'
+  });
+
+  $stateProvider.state('inspection_templates', {
+    url: '/inspection_templates',
+    templateUrl: '/assets/inspection_templates/_index.html',
+    controller: 'InspectionTemplatesCtrl'
+  })
+
+
+});
+
+
+inspectionApp.factory('inspection_templates', [function(){
+  var o = {
+    inspection_templates: [
+      {
+        name: "test1",
+        items: [
+        {
+          name: 'item1',
+          section: 'section1'
+        },
+        {
+          name: 'item2',
+          section: 'section2'
+        }
+      ]
+      },
+      {
+        name: "test2",
+        items: [
+        {
+          name: 'item1',
+          section: 'section1'
+        },
+        {
+          name: 'item2',
+          section: 'section2'
+        }
+      ]
+      }
+    ]
+  };
+  return o;
+}]);
+
+inspectionApp.factory('inspection_template_items', [function(){
+  var o = {
+    items: [
+      {
+        name: "test1"
+      },
+      {
+        name: "test2"
+      }
+    ]
+  };
+  return o;
+}]);
+
 
 inspectionApp.controller('HomeCtrl', ['$scope', function($scope){
   $scope.title = 'Hello world!';
 }]);
 
-inspectionApp.controller('InspectionTemplatesCtrl', ['$scope', function($scope){
-  $scope.inspection_templates = [
-    {
-      name: "test1"
-    },
-    {
-      name: "test2"
-    }
-  ]
+
+inspectionApp.controller('InspectionTemplatesCtrl', [
+  '$scope', 
+  '$stateParams', 
+  'inspection_templates',
+function($scope, $stateParams, inspection_templates) {
+
+  $scope.inspection_templates = inspection_templates.inspection_templates;
+
+  $scope.addInspectionTemplate = function() {
+    $scope.inspection_templates.push({
+      name: $scope.name,
+      items: [
+        {
+          name: 'item1',
+          section: 'section1'
+        },
+        {
+          name: 'item2',
+          section: 'section2'
+        }
+      ]
+    });
+    $scope.name = '';
+  };
+}]);
+
+
+inspectionApp.controller('InspectionTemplateItemsCtrl', [
+  '$scope', 
+  'inspection_template_items',
+  function($scope, items){
+    $scope.items = items.items;
+
+    $scope.addItem = function() {
+      if (!$scope.name || $scope.name === '') {
+        return;
+      }
+      $scope.items.push({
+        name: $scope.name,
+        section: $scope.section
+      });
+      $scope.name = '';
+      $scope.section = '';
+    };
+
+    $scope.toggleEdit = function(item) {
+      if (item.editing) {
+        item.editing = false;
+      } else {
+        item.editing = true;
+      }
+    };
 }]);
