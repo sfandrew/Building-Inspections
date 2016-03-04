97dda4e2cc2a91577510cf6761f3c71bf90e7e7f
james <james@sfrent.net>
2016-03-04 14:19:36 -0800

    Remove default accounts from db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,3 @@
 
 r1 = Role.create({name: "Regular", description: "Can do regular inspections stuff"})
 r2 = Role.create({name: "Admin", description: "Can perform any CRUD operation on any resource"})
-
-u1 = User.create({name: "Sally", email: "sally@example.com", password: "aaaaaaaa", password_confirmation: "aaaaaaaa", role_id: r1.id})
-u2 = User.create({name: "Sue", email: "sue@example.com", password: "aaaaaaaa", password_confirmation: "aaaaaaaa", role_id: r1.id})
-u3 = User.create({name: "Kev", email: "kev@example.com", password: "aaaaaaaa", password_confirmation: "aaaaaaaa", role_id: r1.id})
-u4 = User.create({name: "Jack", email: "jack@example.com", password: "aaaaaaaa", password_confirmation: "aaaaaaaa", role_id: r2.id})
