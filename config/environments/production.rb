b587e14cd8ef3479bbf87b1458055949b0867ef4
james <james@sfrent.net>
2016-03-10 10:27:00 -0800

    Configure for sending emails on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Devise needs this
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'http://quiet-dawn-68087.herokuapp.com/' }
 end
